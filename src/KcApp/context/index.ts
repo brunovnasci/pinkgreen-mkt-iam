32e34c11caaff1bd559b5e1b2a667c83f74f52df
Caio Ventura <37090076+caiovn@users.noreply.github.com>
2023-04-13 20:10:19 -0300

    fix: keycloak login page

--- src/KcApp/context/index.ts
+++ src/KcApp/context/index.ts
@@ -15,7 +15,7 @@ export const { kcContext } = getKcContext<
 		pageId: "error.ftl";
 	}
 >({
-	//"mockPageId": "error.ftl",
+	// "mockPageId": "login.ftl",
 	"mockData": [
 		{
 			"pageId": "register-user-profile.ftl",
